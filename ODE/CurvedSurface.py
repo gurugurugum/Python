917ea360b5c620be31e85f4000b33b66973cb80b
岡 和磨 <kazuma@oka-kazuma-no-MacBook-Pro.local>
2016-05-03 00:52:58 +0900

    changed symbols to sympy's ones

--- ODE/CurvedSurface.py
+++ ODE/CurvedSurface.py
@@ -5,20 +5,23 @@ from mpl_toolkits.mplot3d import Axes3D
 import math
 from sympy import *
 
-x, y=symbols('x y')
+xu=[Symbol('x'+str(i)) for i in range(2)]
 
-gll=[[4*x**2 + 1, 4*x*y], [4*x*y, 4*y**2 + 1]]
+gll=[[4*xu[0]**2 + 1, 4*xu[0]*xu[1]], [4*xu[0]*xu[1], 4*xu[1]**2 + 1]]
 
-def Gammaull(x, y):
-	return [[[-16*x*y**2/(4*x**2 + 4*y**2 + 1) + 4*x*(16*x**2*y**2/((4*x**2 + 1)*(4*x**2 + 4*y**2 + 1)) + 1/(4*x**2 + 1)), 0], [0, -16*x*y**2/(4*x**2 + 4*y**2 + 1) + 4*x*(16*x**2*y**2/((4*x**2 + 1)*(4*x**2 + 4*y**2 + 1)) + 1/(4*x**2 + 1))]], [[-16*x**2*y/(4*x**2 + 4*y**2 + 1) + 4*y*(4*x**2 + 1)/(4*x**2 + 4*y**2 + 1), 0], [0, -16*x**2*y/(4*x**2 + 4*y**2 + 1) + 4*y*(4*x**2 + 1)/(4*x**2 + 4*y**2 + 1)]]]
+guu=Matrix(gll).inv().tolist()
+
+Gammalll=[[[Rational(1,2)*(gll[i][k].diff(xu[j])+gll[i][j].diff(xu[k])-gll[j][k].diff(xu[i])) for k in range(2)] for j in range(2)] for i in range(2)]
+
+Gammaull=[[[sum([guu[i][d]*Gammalll[d][j][k] for d in range(2)]) for k in range(2)] for j in range(2)] for i in range(2)]
 
 def func(y, t):
-	return [-sum([sum([Gammaull(y[2], y[3])[0][i][j]*y[i]*y[j] for i in range(2)]) for j in range(2)]), -sum([sum([Gammaull(y[2], y[3])[1][i][j]*y[i]*y[j] for i in range(2)]) for j in range(2)]), y[0], y[1]]
+	return [-sum([sum([Gammaull[0][i][j].subs(xu[0], y[2]).subs(xu[1], y[3])*y[i]*y[j] for i in range(2)]) for j in range(2)]), -sum([sum([Gammaull[1][i][j].subs(xu[0], y[2]).subs(xu[1], y[3])*y[i]*y[j] for i in range(2)]) for j in range(2)]), y[0], y[1]]
 
 x0=[1, 0]
 theta0=math.pi*3/2
 v0=[math.cos(theta0), math.sin(theta0)]
-mag=sum([sum([gll[d1][d2].subs(x, x0[0]).subs(y, x0[1])*v0[d1]*v0[d2] for d1 in range(2)]) for d2 in range(2)])
+mag=sum([sum([gll[d1][d2].subs(xu[0], x0[0]).subs(xu[1], x0[1])*v0[d1]*v0[d2] for d1 in range(2)]) for d2 in range(2)])
 v0=list(map(lambda n:n/math.sqrt(mag), v0))
 
 y0 = [v0[0], v0[1], x0[0], x0[1]]
@@ -35,4 +38,4 @@ plt.plot(traj[:, 2], traj[:, 3])
 plt.show()
 
 for i in range(0, 10000, 100):
-	print(sum([sum([gll[d1][d2].subs(x, traj[i][2]).subs(y, traj[i][3])*traj[i][d1]*traj[i][d2] for d1 in range(2)]) for d2 in range(2)]))
+	print(sum([sum([gll[d1][d2].subs(xu[0], traj[i][2]).subs(xu[1], traj[i][3])*traj[i][d1]*traj[i][d2] for d1 in range(2)]) for d2 in range(2)]))
