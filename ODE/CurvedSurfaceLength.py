e694e07bfb282c83c0cc23594b141430ca0726c7
岡 和磨 <kazuma@oka-kazuma-no-MacBook-Pro.local>
2016-05-03 13:05:50 +0900

    improved codes

--- ODE/CurvedSurfaceLength.py
+++ ODE/CurvedSurfaceLength.py
@@ -10,34 +10,35 @@ xu = [Symbol('x' + str(i)) for i in range(2)]
 z = sqrt(10000 - xu[0] ** 2 - xu[1] ** 2)
 
 dxu = [Symbol('dx' + str(i)) for i in range(2)]
-
 g = dxu[0] ** 2 + dxu[1] ** 2 + (z.diff(xu[0]) * dxu[0] + z.diff(xu[1]) * dxu[1]) ** 2
-
 gll = [[Rational(1, 2) * g.diff(dxu[i]).diff(dxu[j]) for i in range(2)] for j in range(2)]
-
 guu = Matrix(gll).inv().tolist()
 
 Gammalll = [[[Rational(1,2) * (gll[i][k].diff(xu[j]) + gll[i][j].diff(xu[k]) - gll[j][k].diff(xu[i])) for k in range(2)] for j in range(2)] for i in range(2)]
-
 Gammaull = [[[sum([guu[i][d] * Gammalll[d][j][k] for d in range(2)]) for k in range(2)] for j in range(2)] for i in range(2)]
 
 def func(y, t):
 	return [-sum([sum([Gammaull[0][i][j].subs(xu[0], y[2]).subs(xu[1], y[3]) * y[i] * y[j] for i in range(2)]) for j in range(2)]), -sum([sum([Gammaull[1][i][j].subs(xu[0], y[2]).subs(xu[1], y[3]) * y[i] * y[j] for i in range(2)]) for j in range(2)]), y[0], y[1]]
 
-searchDistance = 100
-step = 0.01
-t = np.arange(0, searchDistance, step)
-
 def firstMinimaOnTraj(goal, traj):
 	beforeDistance = searchDistance ** 2
 	currentDistance = 0.0
-	for	i in range(int(searchDistance / step)):
+	for	i in range(len(traj[:, 0])):
 		currentDistance = math.sqrt((goal[0] - traj[i,2]) ** 2 + (goal[1] - traj[i,3]) ** 2 + (z.subs(xu[0], goal[0]).subs(xu[1], goal[1]) - z.subs(xu[0], traj[i,2]).subs(xu[1], traj[i,3])) ** 2)
 		if currentDistance < beforeDistance:
 			beforeDistance = currentDistance
 		else:
 			return [beforeDistance, i * step]
 	return [-1, -1]
+
+def unitVecOfDirection(theta):
+	v0 = [math.cos(theta), math.sin(theta)]
+	mag = sum([sum([gll[d1][d2].subs(xu[0], start[0]).subs(xu[1], start[1]) * v0[d1] * v0[d2] for d1 in range(2)]) for d2 in range(2)])
+	return list(map(lambda n:n / math.sqrt(mag), v0))
+
+searchDistance = 100
+step = 0.01
+t = np.arange(0, searchDistance, step)
 
 start = [5, 0]
 goal = [9, 1]
@@ -49,10 +50,7 @@ beforeDistance = searchDistance ** 2
 currentDistance = 0.0
 result = 0.0
 for i in range(int(70 / angleStep)):
-	v0 = [math.cos(theta), math.sin(theta)]
-	mag = sum([sum([gll[d1][d2].subs(xu[0], start[0]).subs(xu[1], start[1]) * v0[d1] * v0[d2] for d1 in range(2)]) for d2 in range(2)])
-	v0 = list(map(lambda n:n / math.sqrt(mag), v0))
-
+	v0 = unitVecOfDirection(theta)
 	y0 = v0 + start
 	traj = odeint(func, y0, t)
 	
@@ -70,17 +68,22 @@ for i in range(int(70 / angleStep)):
 		theta += angleStep
 print(result)
 
-#height = [z.subs(xu[0], a).subs(xu[1], b) for a, b in zip(traj[:, 2], traj[:, 3])]
+v0 = unitVecOfDirection(theta)
+y0 = v0 + start
+t = np.arange(0, result, step)
+traj = odeint(func, y0, t)
+
+height = [z.subs(xu[0], a).subs(xu[1], b) for a, b in zip(traj[:, 2], traj[:, 3])]
 #height = list(map(lambda a, b:z.subs(xu[0], a).subs(xu[1], b), traj[:, 2], traj[:, 3]))
 #height = list(map(lambda n:z.subs(xu[0], m), traj[:, 2]))
 #height = list(map(lambda n:z.subs(xu[0], m), traj[:, 2]))
 #height = z.subs(xu[0], traj[:, 2]).subs(xu[1], traj[:, 3])
 
-#fig = plt.figure()
-#ax = fig.gca(projection = '3d')
-#ax.plot(traj[:, 2], traj[:, 3], height[:])
+fig = plt.figure()
+ax = fig.gca(projection = '3d')
+ax.plot(traj[:, 2], traj[:, 3], height[:])
 #plt.plot(traj[:, 2], traj[:, 3])
-#plt.show()
+plt.show()
 
 #for i in range(0, 10000, 100):
 #	print(sum([sum([gll[d1][d2].subs(xu[0], traj[i][2]).subs(xu[1], traj[i][3]) * traj[i][d1] * traj[i][d2] for d1 in range(2)]) for d2 in range(2)]))
