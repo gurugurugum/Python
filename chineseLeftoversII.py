0e409d69ddea18a87085f2584fd8e71e168a788c
岡 和磨 <kazuma@oka-kazuma-no-MacBook-Pro.local>
2016-06-18 09:39:52 +0900

    changed a bit

--- chineseLeftoversII.py
+++ chineseLeftoversII.py
@@ -18,13 +18,13 @@ def mul_inv(a, b):
 	b0 = b
 	x0, x1 = 0, 1
 	if b == 1: return 1
-	while a > 1:
+	while b > 0:
 		#		q = int(a / b)
 		q = a // b
 		#		print(q)
 		a, b = b, a%b
 		x0, x1 = x1 - q * x0, x0
-	if x1 < 0: x1 += b0
+	#	if x1 < 0: x1 += b0
 	return x1
 
 def A(n):
@@ -35,6 +35,37 @@ def A(n):
 		numbers.append(i)
 		primes.append(prime(i))
 	return chinese_remainder(primes,numbers)
+
+def Sl(n):
+	rl = []
+	i = 1
+	Al = [A(i)]
+	p = prime(i)
+	while p <= n:
+		#		print(i, p)
+		for j in range(len(Al) - 1):
+			if Al[j] % p == 0:
+				rl.append((p,j+1,Al[j]))
+				break
+		i += 1
+		p = prime(i)
+		Al.append(A(i))
+	return rl
+
+def S(n):
+	sum = 0
+	i = 1
+	Al = [A(i)]
+	p = prime(i)
+	while p <= n:
+		for j in range(len(Al) - 1):
+			if Al[j] % p == 0:
+				sum += p
+				break
+		i += 1
+		p = prime(i)
+		Al.append(A(i))
+	return sum
 
 if __name__ == '__main__':
 	sum = 0
