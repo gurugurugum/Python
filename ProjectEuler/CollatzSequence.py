9d506f7b79a9d5b1cc52b3d3165d9ccac2227445
k.oka <k.oka@zuken.co.jp>
2016-07-04 13:30:11 +0900

    fixed typos

--- ProjectEuler/CollatzSequence.py
+++ ProjectEuler/CollatzSequence.py
@@ -1,11 +1,11 @@
-def NCSec(n):
+def NCSeq(n):
 	if n % 2 == 0:
 		return n // 2
 	else:
 		return 3 * n +1 
 
-def CSec(n):
+def CSeq(n):
 	res=[n]
 	while res[-1] != 1:
-		res.append(NCSec(res[-1]))
+		res.append(NCSeq(res[-1]))
 	return res
